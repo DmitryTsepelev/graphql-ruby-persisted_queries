0a096e677b8e947b05be3e249645d150b08ba2c0
Brian Morton <bmorton@microsoft.com>
2020-01-30 20:04:22 -0800

    Wire up ErrorHandlers feature into Resolver

--- lib/graphql/persisted_queries.rb
+++ lib/graphql/persisted_queries.rb
@@ -8,11 +8,13 @@ require "graphql/persisted_queries/version"
 module GraphQL
   # Plugin definition
   module PersistedQueries
-    def self.use(schema_defn, store: :memory, hash_generator: :sha256, **options)
+    def self.use(schema_defn, store: :memory, hash_generator: :sha256,
+                 error_handler: :default, **options)
       schema = schema_defn.is_a?(Class) ? schema_defn : schema_defn.target
 
       schema.singleton_class.prepend(SchemaPatch)
       schema.hash_generator = hash_generator
+      schema.configure_persisted_query_error_handler(error_handler)
       schema.configure_persisted_query_store(store, options)
     end
   end
--- spec/graphql/persisted_queries/schema_patch_spec.rb
+++ spec/graphql/persisted_queries/schema_patch_spec.rb
@@ -13,8 +13,17 @@ RSpec.describe GraphQL::PersistedQueries::SchemaPatch do
     end
   end
 
+  TestErrorHandler = Class.new(GraphQL::PersistedQueries::ErrorHandlers::BaseErrorHandler) do
+    attr_accessor :last_handled_error
+
+    def call(error)
+      self.last_handled_error = error
+      raise error
+    end
+  end
+
   GraphqlSchema = Class.new(GraphQL::Schema) do
-    use GraphQL::PersistedQueries
+    use GraphQL::PersistedQueries, error_handler: TestErrorHandler.new({})
 
     query QueryType
   end
@@ -51,6 +60,40 @@ RSpec.describe GraphQL::PersistedQueries::SchemaPatch do
 
     it "returns data" do
       expect(response["data"]).to eq("someData" => "some value")
+    end
+  end
+
+  context "when cache is unavailable" do
+    let(:query) { nil }
+    let(:sha256) { 1 }
+
+    UnavailableStore = Class.new(GraphQL::PersistedQueries::StoreAdapters::BaseStoreAdapter) do
+      def fetch_query(_)
+        raise "Store unavailable"
+      end
+
+      def save_query(_, _)
+        raise "Store unavailable"
+      end
+    end
+
+    around do |test|
+      original_store = GraphqlSchema.persisted_query_store
+      GraphqlSchema.configure_persisted_query_store(UnavailableStore.new({}), {})
+      test.run
+      GraphqlSchema.configure_persisted_query_store(original_store, {})
+    end
+
+    it "calls the error handler" do
+      # rubocop: disable Lint/HandleExceptions
+      begin
+        perform_request
+      rescue RuntimeError
+        # Ignore the expected error
+      end
+      # rubocop: enable Lint/HandleExceptions
+
+      expect(GraphqlSchema.persisted_query_error_handler.last_handled_error).to be_a(RuntimeError)
     end
   end
 end
